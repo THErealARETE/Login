47485fb9eff1c180931265b6d35d3b1a5ba33aa6
Amuzu Imuse <Arete>
2019-09-18 03:27:33 +0100

    Updates

---
+++ precache-manifest.1e1c3fe5ef5dfab3c31865abb480811f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c50b8a6b725cfdfbd6f7",
+    "url": "/THErealARETE/Login.git/static/js/runtime~main.c50b8a6b.js"
+  },
+  {
+    "revision": "cd21516c76960b97a634",
+    "url": "/THErealARETE/Login.git/static/js/main.cd21516c.chunk.js"
+  },
+  {
+    "revision": "adfda23623ae9fe6f681",
+    "url": "/THErealARETE/Login.git/static/js/1.adfda236.chunk.js"
+  },
+  {
+    "revision": "cd21516c76960b97a634",
+    "url": "/THErealARETE/Login.git/static/css/main.93df2c74.chunk.css"
+  },
+  {
+    "revision": "bef079d4c9b70836472804afd8a7e484",
+    "url": "/THErealARETE/Login.git/index.html"
+  }
+];
